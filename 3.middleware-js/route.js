2737ab51fe0dda091ab0633319527bfc45c13ab7
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2026-01-30 03:03:53 +0530

    routes in middleware

--- 3.middleware-js/route.js
+++ 3.middleware-js/route.js
@@ -1,18 +1,71 @@
 const express = require('express')
-const app = express()
 const router = express.Router()
 
-// predicate the router with a check and bail out when needed
-router.use((req, res, next) => {
-  if (!req.headers['x-auth']) return next('router')
-  next()
+// 1. authentication checked middleware
+const auth = function (req, res, next) {
+  console.log("I am inside auth wala middleware");
+  
+
+  // for your help i create a dummy user addEventListener
+
+  req.user = {userId: 1, name: "John Doe"}
+
+  if(req.user){
+    // if a valid user is there in req, then proceed to next middleware
+    next()
+  } else {
+    
+    // if not a valid user
+    res.json({
+      success: false,
+      message: "User not authenticated"
+    })
+}
+}
+
+// 2. checked role of user student or not
+const isStudent = function (req, res, next) {
+  console.log("I am inside isStudent wala middleware");
+
+  if(req.user.role === 'student'){
+    next()
+  } else {
+    res.json({
+      success: false,
+      message: "User is not a student"
+    })
+  }
+
+}
+
+// 3. checked role of user admin or not
+
+const isAdmin = function (req, res, next) {
+  console.log("I am inside  is admin wala");
+
+  if(req.user.role === 'admin'){
+    next()
+  }
+  else {
+    res.json({
+      success: false,
+      message: "User is not an admin"
+    })
+  }
+  
+}
+
+// routes
+
+router.get("/student", auth, isStudent,(req, res)=> {
+  console.log("I AM INSIDE STUDENT route");
+  res.send("student specific data");
+  
 })
 
-router.get('/user/:id', (req, res) => {
-  res.send('hello, user!')
+router.get("/admin", auth, isAdmin, (req, res) => {
+  console.log("I AM INSIDE ADMIN route");
+  res.send("admin specific data");
 })
 
-// use the router and 401 anything falling through
-app.use('/admin', router, (req, res) => {
-  res.sendStatus(401)
-})
+module.exports = router
