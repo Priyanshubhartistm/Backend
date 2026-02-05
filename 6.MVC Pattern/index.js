499c58e28582b30392b809dfd0a800738179cce5
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2026-02-06 00:24:16 +0530

    mvc

--- 6.MVC Pattern/index.js
+++ 6.MVC Pattern/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+const productRoutes = require('./routes/productRouts');
 
 const app = express();
+
+// load dev
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 // Connect to the database
 connectDB();
@@ -9,6 +16,8 @@ connectDB();
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.use('/api', productRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
